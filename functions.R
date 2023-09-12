cc71ca46e829b03264cc48e0e5fbdb0ab4cb8d53
riinuots <riinuots@gmail.com>
2023-09-12 08:58:26 +0100

    procedure events added

--- functions.R
+++ functions.R
@@ -1,3 +1,13 @@
+# Format in-text numbers with thousands separated by ,
+library(knitr)
+inline_hook = function(x) {
+  if (is.numeric(x)) {
+    format(x,big.mark = ",")
+  } else x
+}
+knitr::knit_hooks$set(inline = inline_hook)
+
+# DataTable function (HTML output only)
 mydt =   function(df, pagelength = 20){
   DT::datatable(df,
                 rownames = FALSE,
